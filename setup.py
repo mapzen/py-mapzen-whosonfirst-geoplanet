c7e217f53164967693e2d8b4f0a352294d059444
thisisaaronland <aaron@mapzen.com>
2015-09-29 23:06:45 +0000

    add proper dependency link for mz.wof.names; bump to 0.03

--- setup.py
+++ setup.py
@@ -8,18 +8,19 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.geoplanet',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.geoplanet'],
-    version='0.02',
+    version='0.03',
     description='Simple Python wrapper for Who\'s On First GeoPlanet-related functions',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-geoplant',
     install_requires=[
-        "mapzen.whosonfirst.labels"
+        "mapzen.whosonfirst.names"
         ],
     dependency_links=[
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-names/tarball/master#egg=mapzen.whosonfirst.names-0.01',
         ],
     packages=packages,
     scripts=[
         'scripts/wof-append-geoplanet-names'
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-geoplanet/releases/tag/v0.02',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-geoplanet/releases/tag/v0.03',
     license='BSD')
